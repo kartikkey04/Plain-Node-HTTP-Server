7a54de282ab34fa5c7684a29bd24bb66582a5845
kartikey <kartikkey0414@gmail.com>
2025-04-09 11:36:58 +0530

    basic of req and res handled

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const http = require('http');
 
 const PORT = 3000;  // we defined a variable to store the value of the port.
 
-const server = http.createServer();  // creating a new server instance, but it is not running
+const server = http.createServer( async(req, res) => {
+    // whenever any request hit my server, this function will be called
+
+    console.log('Request received');
+
+    if(req.method == 'GET'){
+        res.end('GET request received');
+    }else if(req.method == 'POST'){
+        // In post request, let's send the response code as 201
+        res.writeHead(201);
+        res.end('POST request received');
+    }else{
+        res.end('Hello World!!')
+    }
+
+});  // creating a new server instance, but it is not running
 
 server.listen(PORT, ()=>{
     // this function will be called when server is started
